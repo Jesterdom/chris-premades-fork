40c910f0866b9bf5aa65fe23e1f33a2f8133b10e
Tom Freeman <tfreeman@wsu.edu>
2025-10-01 20:35:35 -0700

    First test
    Fix repellingblast

--- scripts/macros/2014/classFeatures/warlock/invocations/repellingBlast.js
+++ scripts/macros/2014/classFeatures/warlock/invocations/repellingBlast.js
@@ -1,13 +1,21 @@
 import {activityUtils, dialogUtils, genericUtils, itemUtils, tokenUtils, workflowUtils} from '../../../../../utils.js';
 
 async function late({trigger: {entity: item}, workflow}) {
-    if (activityUtils.getIdentifier(workflow.activity) !== 'eldritchBlastBeam') return;
+     if (activityUtils.getIdentifier(workflow.activity) !== 'eldritchBlast') return;
     if (!workflow.hitTargets.size) return;
-    let targetToken = workflow.targets.first();
-    let selection = await dialogUtils.confirm(item.name, genericUtils.format('CHRISPREMADES.Dialog.UseOn', {itemName: item.name, tokenName: targetToken.name}));
+    let validTargets = Array.from(workflow.hitTargets.filter(i => tokenUtils.getDistance(workflow.token, i) > 5));
+    if (!validTargets.length) return;
+    let selection = await dialogUtils.confirm(item.name, genericUtils.format('CHRISPREMADES.Dialog.Use', {itemName: item.name}));
     if (!selection) return;
+    let target;
+    if (validTargets.length > 1) {
+        selection = await dialogUtils.selectTargetDialog(item.name, 'CHRISPREMADES.Macros.RepellingBlast.Select', validTargets);
+        if (!selection?.length) return;
+        target = selection[0];
+    }
+    if (!target) target = validTargets[0];
     await workflowUtils.completeItemUse(item);
-    await tokenUtils.pushToken(workflow.token, targetToken, 10);
+    await tokenUtils.pushToken(workflow.token, target, 10);
 }
 export let repellingBlast = {
     name: 'Eldritch Invocations: Repelling Blast',
